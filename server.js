8978ef9e8249cc65a5f4aa94c927e7363ca6b0ec
= <=>
2015-04-01 17:56:06 -0400

    testing server-side

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendfile('./public/index.html');
+	// res.sendfile('./public/index.html');
+	res.send('Helo World');
 });
 
+app.get('/story', function(req, res) {
+	res.send("<p> hey " + req.query.name + " yolo </p>");
+});
 app.listen(app.get('port'), function() {
 	console.log('Fuck it ship it.');
 });
