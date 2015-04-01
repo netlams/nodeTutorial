4a9239ce6922ad667bceb74ed6db9b04c14111e5
= <=>
2015-04-01 18:28:12 -0400

    server.js upping boilder

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	// res.sendfile('./public/index.html');
-	res.send('Helo World');
+	res.sendfile('./public/index.html');
 });
 
 app.get('/story', function(req, res) {
 	res.send("<p> hey " + req.query.name + " yolo </p>");
 });
+
+app.get('/boiler', function(req, res) {
+	res.sendfile('./boiler/index.html');
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Fuck it ship it.');
 });
+
+
+/*slides.com/dancadden/deck-2/*/
